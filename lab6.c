c439b300f03de14dc7bd22f4d3df29484eb45bf9
jcho <jakeco123@gmail.com>
2020-02-20 18:11:13 -0800

    lab6

--- lab6.c
+++ lab6.c
@@ -1,24 +1,54 @@
 //Jung Min Cho
 //csc251
-//02/10/2020
+//02/19/2020
 //lab6
+
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include<time.h>	
+#define SIZE 5
 
-int main(void)
-{	
-	int x=0,y=0,z=0,t;
-	//printf("Enter a seed \n");
-	//scanf("%d" ,&y);
-	srand(time(NULL));
-	for ( t=0; t<10; t++)
-   	{
-		x=rand()%10;
-		printf("z=%d y=%d x=%d \n",z,y,x);
-	}
+void swaparr(int *arr1, int *arr2, int s)
+{
+	int i=0, temp = 0;
 	
-	return EXIT_SUCCESS;
+	for(i = 0; i<s; i++)
+	{
+		temp = arr1[i];
+		arr1 [i] = arr2[i];
+		arr2 [i] = temp;
+	}
 }
- 
+
+int main ()
+{
+	int i =0;
+	
+	int array1 [SIZE] = {0,2,4,6,8};
+	int array2 [SIZE] = {1,3,5,7,9};
+
+
+	printf ("Before Swap \n");
+	
+	for (i=0; i <SIZE; i++)
+	{
+		printf("firstArray[%d] = %d", i, array1[i]);
+		printf(", secondArray[%d] = %d \n ", i, array2[i]);
+	}	
+
+	
+	swaparr(array1, array2, SIZE);
+
+	printf ("After Swap \n");
+
+	for (i=0; i<SIZE; i++)
+	{
+		printf("firstArray[%d] = %d",i,array1[i]);
+		printf(", secondArray[%d] = %d \n", i ,array2[i]);
+	}	
+	return 0;
+}
+
+	
+
